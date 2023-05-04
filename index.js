77cb012fc89d709b2b2dbc6320db505c8f2eaf8b
Dennis Demir <dennisjdemir@gmail.com>
2023-05-04 00:46:36 -0400

    change flex layout to display 5 cards per row

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function createAnimeCard(anime){
     close.classList.add('close-div')
     btn.classList.add('close-button')
     btn.innerHTML='x'
+    title.classList.add('anime-title')
     animeCard.append(close);
     close.append(btn);
     animeCard.append(img);
@@ -32,10 +33,24 @@ function createAnimeCard(anime){
     btn.textContent='X'
     img.src=`${anime.images.jpg.image_url}`;
     title.innerHTML=`Title: ${anime.title_english}`
-    score.innerHTML=`Score: ${anime.score}`    
+    score.innerHTML=`Score: ${anime.score}`   
     airedFrom.innerHTML=`First Aired: ${anime.aired.from}`
     airedTo.innerHTML=`Aired Until: ${anime.aired.to}`
     document.querySelector('#anime-container').appendChild(animeCard)
 }
 
-getTopAnimes(page)
+function handleSubmit(e){
+    e.preventDefault()
+    input = document.querySelector('input#searchByName')
+    
+    fetch(`https://api.jikan.moe/v4/anime?q=${input.value}&sfw`)
+    .then(resp =>resp.json())
+    .then(data => {
+        document.querySelector('#anime-container').innerHTML = ''
+        data.data.forEach(anime=>createAnimeCard(anime))
+    })
+}
+
+document.querySelector('#search-bar').addEventListener('submit',handleSubmit)
+
+    getTopAnimes(page)
